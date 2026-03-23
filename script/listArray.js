72c6e4f0b8d5e5aced7f87ecf3d5e38626903f79
연진주 <jinjuyeon3@gmail.com>
2026-03-23 11:37:34 +0900

    list 기능 추가

---
+++ script/listArray.js
@@ -0,0 +1,36 @@
+let listArray = [
+    {
+        pid:0,
+        pName:"Matcha Herb Latte",
+        pDesc:"깊고 부드러운 말차에 허브 향을 더한, Cafe Pause만의 시그니처 메뉴",
+        pPrice:5200,
+        pThumbnail:"list-matcha-latte.jpg",
+        pLike:false
+    },
+    {
+        pid:1,
+        pName:"Vanilla Bean Latte",
+        pDesc:"깊게 추출한 에스프레소에 마다가스카르 바닐라 빈의 은은한 단맛을 더한 라떼",
+        pPrice:4800,
+        pThumbnail:"list-vanilla-bean-latte.jpg",
+        pLike:false
+    },
+    {
+        pid:2,
+        pName:"Earl Grey Cream",
+        pDesc:"진한 얼그레이 티에 가볍게 올라간 크림과 과하지 않은 시트러스 향",
+        pPrice:4600,
+        pThumbnail:"list-earl-grey-cream.jpg",
+        pLike:false
+    },
+    {
+        pid:3,
+        pName:"Lemon Chamomile",
+        pDesc:"캐모마일 티 베이스에 상큼한 레몬을 더한 티 블렌드",
+        pPrice:4600,
+        pThumbnail:"list-lemon-chamomile.jpg",
+        pLike:false
+    },
+        
+
+]
