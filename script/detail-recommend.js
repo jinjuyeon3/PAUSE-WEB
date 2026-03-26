47d34ff129cdb29244f141e5e37cfa0dd7e88d52
연진주 <jinjuyeon3@gmail.com>
2026-03-26 14:22:42 +0900

    상품상세페이지4

--- script/detail-recommend.js
+++ script/detail-recommend.js
@@ -1,29 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-    // ul태그에 li태그의 형태로 상품 데이터를 넣은 소스코드
-    const list = document.querySelector(".list-grid");
-    let result = "";
+document.addEventListener("DOMContentLoaded",()=>{
 
-    listArray.forEach(item => {
+    // ul태그에 li태그의 형태로 상품 데이터를 넣는 소스코드
+    const list = document.querySelector(".list-grid")
+    let result = ""
+    listArray.forEach(item=>{
         result += `
-            <li>
-                <a href="#">
-                    <figure>
-                        <img src="./img/${item.pThumbnail}" alt="${item.pName}">
-                    </figure>
-                    <div class="list-option">
-                        <span class="btn-heart">
-                            <img src="./img/list-heart.svg" alt="좋아요 아이콘">
-                        </span>
-                    </div>
-                    <h4>${item.pName}</h4>
-                    <p class="fwm">${item.pDesc}</p>
-                    <div class="price fwb">${toWon(item.pPrice)}</div>
-                </a>
-            </li>
-        `;
-    });
-    list.innerHTML = result;
+        <li>
+            <a href="./detail.html?pid=${item.pid}">
+                <figure>
+                    <img src="./img/${item.pThumbnail}" alt="${item.pName}">
+                </figure>
+                <div class="list-option">
+                    <span class="btn-heart">
+                        <img src="./img/list-heart.svg" alt="좋아요 아이콘">
+                    </span>
+                </div>
+                <h4>${item.pName}</h4>
+                <p class="fwm">${item.pDesc}</p>
+                <div class="price fwb">${toWon(item.pPrice)}</div>
+            </a>
+        </li>`
+    })
+    list.innerHTML = result
 
-    
-});
+})
+
+
+
---
+++ script/detail.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    let productId = getParam("pid")
+    const result = listArray.find(item=>item.pid == productId)
+    // find함수는 앞의 listArray 라는 배열의 여러 원소들을 전부 조사하여 원소 중 pid키값이 현재 productId변수에 저장된 수와 일치되는 원소를 리턴하여 result라는 변수에 저장한다.
+
+    console.log(result)
+    
+
+    const product1 = document.querySelector(".product1")
+    product1.innerHTML = `<div class="common-frame">
+                <figure>
+                    <img src="./img/${result.pThumbnail}" alt="${result.pName}">
+                </figure>
+                <div class="txt-pro">
+                    <h1>${result.pName}</h1>
+                    <P class="fwm">${result.pDesc}</P>
+                    <dl class="pro-table"> 
+                        <dt>판매가</dt>
+                        <dd>${toWon(result.pPrice)}원</dd>
+                        <dt>배송비</dt>
+                        <dd>
+                            2,000원
+                            <span>(20,000원 이상 구매 시 무료)</span>
+                        </dd>
+                    </dl>
+                    <div class="sell-info">
+                        <div class="fwb">${result.pName}</div>
+                        <div>
+                            <div class="box-number">
+                                <span class="btn-minus">
+                                    <img src="./img/btn-pro-minus.svg" alt="감소 버튼">
+                                </span>
+                                <span class="current-count">1</span>
+                                <span class="btn-plus">
+                                    <img src="./img/btn-pro-plus.svg" alt="증가 버튼">
+                                </span>
+                            </div>
+                        </div>
+                        <div class="fwb">${toWon(result.pPrice)}원</div>
+                    </div>
+                    <dl class="price-result">
+                        <dt>전체금액</dt>
+                        <dd class="fwb">${toWon(result.pPrice)}원</dd>
+                    </dl>
+                    <ul class="btns-buy">
+                        <li><a href="#"><img src="./img/list-heart.svg" alt="관심상품 등록 버튼"></a></li>
+                        <li><a href="#">장바구니</a></li>
+                        <li><a href="#">구매하기</a></li>
+                    </ul>
+                </div>
+            </div>`
+
+})
